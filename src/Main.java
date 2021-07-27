6e0053f9bf8bc1ec3fa36b9eb2cc03026e6e62be
Semenova Natalya <semnazl@gmail.com>
2021-07-27 14:02:49 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
             // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-            String number = "30074083397868";
+            String number = "4740176071179787565";
             System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
         }
 
